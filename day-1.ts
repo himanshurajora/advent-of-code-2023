eb495eb98f8c80ac62b40cb28f16fa359efb7ea7
Himanshu Jangid <himanshujhar@gmail.com>
2024-01-19 01:13:40 +0530

    wip day 1 task two

--- day-1.ts
+++ day-1.ts
@@ -1,11 +1,60 @@
 import { DayOneInput } from "./inputs/day-1";
 
 const digits = "0123456789";
+const digitWords = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const digitStrings = Object.keys(digitWords);
+
 export function getCalibrationValue(input: string) {
   let firstDigit;
   let lastDigit;
   for (let i = 0; i < input.length; i++) {
     const digit = input[i];
+    if (firstDigit === undefined && digits.includes(digit)) {
+      firstDigit = digit;
+    }
+
+    if (digits.includes(digit)) {
+      lastDigit = digit;
+    }
+  }
+
+  return +`${firstDigit}${lastDigit}`;
+}
+
+export function getCalibrationValueWithWords(input: string) {
+  let firstDigit;
+  let lastDigit;
+  let sequence = "";
+  for (let i = 0; i < input.length; i++) {
+    let digit: string = input[i];
+    sequence += digit;
+
+    if (
+      digitStrings.some((digitString) => {
+        const doesInclude = sequence.includes(digitString);
+        if (doesInclude) {
+          sequence = digitString;
+        }
+        return doesInclude;
+      })
+    ) {
+      digit =
+        digitWords[sequence as unknown as keyof typeof digitWords].toString();
+      sequence = "";
+    }
+
     if (firstDigit === undefined && digits.includes(digit)) {
       firstDigit = digit;
     }
@@ -24,5 +73,13 @@ export function sumOfCalibrationValues(lines: string[]) {
   }, 0);
 }
 
+export function sumOfCalibrationValuesWithWords(lines: string[]) {
+  return lines.reduce((sum, line) => {
+    return sum + getCalibrationValueWithWords(line);
+  }, 0);
+}
+
 const result1 = sumOfCalibrationValues(DayOneInput);
-console.log(`result of day 1's task one is ${result1}`);
+const result2 = sumOfCalibrationValuesWithWords(DayOneInput);
+console.log(`result of day 1's task 1 is ${result1}`);
+console.log(`result of day 1's task 2 is ${result2}`);
--- tests/day-1.test.ts
+++ tests/day-1.test.ts
@@ -1,8 +1,18 @@
 import { describe, expect, test } from "bun:test";
-import { getCalibrationValue, sumOfCalibrationValues } from "../day-1";
+import {
+  getCalibrationValue,
+  getCalibrationValueWithWords,
+  sumOfCalibrationValues,
+  sumOfCalibrationValuesWithWords,
+} from "../day-1";
 
 const input1 = "2911threeninesdvxvheightwobm";
 const input2 = "3three16xsxhpnqmzmnine8one";
+const input3 = "sshnxbjrt1";
+const input4 = "onefivethreefive";
+const input5 = "one3pcmponehgcqqkrbplzbfivesevenhdzfcsl";
+const input6 = "zoneight234";
+const input7 = "two1nine";
 
 describe("All day 1's task 1 tests should pass", () => {
   test(`${input1} should output 21`, () => {
@@ -11,5 +21,50 @@ describe("All day 1's task 1 tests should pass", () => {
 
   test(`sum of ${input1} and ${input2} should be 59`, () => {
     expect(sumOfCalibrationValues([input1, input2])).toBe(59);
+  });
+});
+
+describe("All day 1's task 2 tests should pass", () => {
+  test(`${input1} should output 28`, () => {
+    expect(getCalibrationValueWithWords(input1)).toBe(28);
+  });
+
+  test(`${input3} should output 11`, () => {
+    expect(getCalibrationValueWithWords(input3)).toBe(11);
+  });
+
+  test(`${input4} should output 15`, () => {
+    expect(getCalibrationValueWithWords(input4)).toBe(15);
+  });
+
+  test(`${input5} should output 17`, () => {
+    expect(getCalibrationValueWithWords(input5)).toBe(17);
+  });
+
+  test(`${input6} should output 14`, () => {
+    expect(getCalibrationValueWithWords(input6)).toBe(14);
+  });
+
+  test(`${input7} should output 29`, () => {
+    expect(getCalibrationValueWithWords(input7)).toBe(29);
+  });
+
+  test("should pass", () => {
+    expect(getCalibrationValueWithWords("eightwothree")).toBe(83);
+  });
+
+  test("should pass", () => {
+    expect(getCalibrationValueWithWords("xtwone3four")).toBe(24);
+  });
+  test("should pass", () => {
+    expect(getCalibrationValueWithWords("abcone2threexyz")).toBe(13);
+  });
+
+  test("should pass", () => {
+    expect(getCalibrationValueWithWords("4nineeightseven2")).toBe(42);
+  });
+
+  test(`sum of ${input1} and ${input2} should be 59`, () => {
+    expect(sumOfCalibrationValuesWithWords([input1, input2])).toBe(59);
   });
 });
