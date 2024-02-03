96bbdf1955e110797de1e4230c2c06f9a1c360bf
Himanshu Jangid <himanshujhar@gmail.com>
2024-02-03 23:29:23 +0530

    day 3 part 2 and the amazing yield use case

--- src/day-3.ts
+++ src/day-3.ts
@@ -5,6 +5,7 @@ export function day3ConvertToArray(input: string) {
 }
 
 export const day3Symbols = `!@#$%^&*()_-+=[]{};:'",<>/?\\|`;
+export const day3Star = `*`;
 
 function hasNeighboringSymbol(matrix: string[][], i: number, j: number) {
   return (
@@ -17,6 +18,54 @@ function hasNeighboringSymbol(matrix: string[][], i: number, j: number) {
     day3Symbols.includes(matrix[i + 1]?.[j + 1]) ||
     day3Symbols.includes(matrix[i]?.[j + 1])
   );
+}
+
+function* hasNeighboringStar(matrix: string[][], i: number, j: number) {
+  yield day3Star.includes(matrix[i - 1]?.[j + 1]) && [i - 1, j + 1];
+  yield day3Star.includes(matrix[i - 1]?.[j]) && [i - 1, j];
+  yield day3Star.includes(matrix[i - 1]?.[j - 1]) && [i - 1, j - 1];
+  yield day3Star.includes(matrix[i]?.[j - 1]) && [i, j - 1];
+  yield day3Star.includes(matrix[i + 1]?.[j - 1]) && [i + 1, j - 1];
+  yield day3Star.includes(matrix[i + 1]?.[j]) && [i + 1, j];
+  yield day3Star.includes(matrix[i + 1]?.[j + 1]) && [i + 1, j + 1];
+  yield day3Star.includes(matrix[i]?.[j + 1]) && [i, j + 1];
+}
+
+export function day3FindAdjacentStar(matrix: string[][]) {
+  const stars: Record<string, number[]> = {};
+  for (let i = 0; i < matrix.length; i++) {
+    let currentSequence = "";
+    for (let j = 0; j < matrix[i].length; j++) {
+      const n = matrix[i][j];
+      if (!isNaN(+n)) {
+        currentSequence += n;
+      } else {
+        currentSequence = "";
+      }
+
+      if (!day3Symbols.includes(matrix[i][j]) && matrix[i][j] !== ".") {
+        const neighboringStars = Array.from(hasNeighboringStar(matrix, i, j));
+        if (neighboringStars.some((star) => star)) {
+          while (!isNaN(+matrix[i][++j])) {
+            currentSequence += matrix[i][j];
+          }
+          neighboringStars.forEach((star) => {
+            if (star && currentSequence) {
+              if (stars[`[${star[0]},${star[1]}]`]) {
+                stars[`[${star[0]},${star[1]}]`].push(+currentSequence);
+              } else {
+                stars[`[${star[0]},${star[1]}]`] = [+currentSequence];
+              }
+            }
+          });
+          currentSequence = "";
+        }
+      }
+    }
+    currentSequence = "";
+  }
+
+  return stars;
 }
 
 export function day3FindAdjacentNumbers(matrix: string[][]): number[] {
@@ -63,3 +112,15 @@ console.log(
     0
   )}`
 );
+
+const result = day3FindAdjacentStar(matrix);
+
+// only filter with two or more stars
+const filtered = Object.values(result)
+  .filter((v) => v.length === 2)
+  .map((v) => {
+    console.log(v[0], v[1], v[0] * v[1]);
+    return v[0] * v[1];
+  })
+  .reduce((p, c) => p + c, 0);
+console.log(filtered);
